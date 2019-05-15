79790556b982ba8169e081fc1a3282405dca773e
Unknown <dreed12@mail.pima.edu>
2019-05-14 22:48:51 -0700

    finishing touches (The beginning)

---
+++ Assets/Scripts/Player/ShaderInteraction.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderInteraction : MonoBehaviour
+{
+	#region Variables
+
+	public bool Edged;
+
+	public Color Edge, Normal;
+
+	public Material PlayerMat;
+	
+	#endregion
+	
+	#region Unity Functions
+	
+	private void Awake()
+	{
+		
+	}
+	
+	private void Start()
+	{
+       
+	}
+
+    
+	private void Update()
+	{
+		if (transform.position.x >= 8.5f || transform.position.x <= -8.5f) 
+		{
+			Edged = true;
+		}
+		else if (transform.position.y >= 8.5f || transform.position.y <= -8.5f)
+		{
+			Edged = true;
+		}
+		else
+		{
+			Edged = false;
+		}
+			
+
+		ChangePlayerColor(Edged);
+	}
+
+	#endregion
+
+	#region My Functions
+
+	private void ChangePlayerColor(bool edged)
+	{
+		if (edged)
+		{
+			PlayerMat.SetColor("Color_E24EE7F2", Edge);
+		}
+		else
+		{
+			PlayerMat.SetColor("Color_E24EE7F2", Normal);
+		}
+	}
+
+	#endregion
+}
--- Assets/Scripts/UI/UI_Controller.cs
+++ Assets/Scripts/UI/UI_Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_Controller : MonoBehaviour
 {
@@ -27,12 +28,32 @@ public class UI_Controller : MonoBehaviour
 	{
         
 	}
-	
+
 	#endregion
-	
+
 	#region My Functions
-	
-	
-	
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("Level",LoadSceneMode.Single);
+		SceneManager.LoadScene("Level Environment", LoadSceneMode.Additive);
+		
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Pause()
+	{
+		
+	}
+
+	public void Resume()
+	{
+
+	}
+
 	#endregion
 }
