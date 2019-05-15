68a9102ecca59c4c38a3aea47c625c4ec66cb9e2
Unknown <dreed12@mail.pima.edu>
2019-05-14 19:26:26 -0700

    particle systems made and controller started

--- Assets/Scripts/Environment/ParticleController.cs
+++ Assets/Scripts/Environment/ParticleController.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class ParticleController : MonoBehaviour
 {
 	#region Variables
-	
-	
+
+	[Tooltip("The particle system children on this game object")]
+	public List<ParticleSystem> ParticleEffects;
+
+	[Range(0, 4), Tooltip("Number of Active Particle Systems")]
+	public int NumberON;
+
+	[Range(0,5),Tooltip("How long between random Switches of how many are on")]
+	public float SwitchTime;
 	
 	#endregion
 	
 	#region Unity Functions
 	
-	private void Awake()
-	{
-		
-	}
-	
 	private void Start()
 	{
-       
+		foreach(ParticleSystem ps in ParticleEffects)
+		{
+			ps.gameObject.SetActive(false);
+		}
+
+		StartCoroutine("RandomActivate");
 	}
 
     
@@ -32,7 +39,20 @@ public class ParticleController : MonoBehaviour
 	
 	#region My Functions
 	
-	
+	private IEnumerator RandomActivate()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(SwitchTime);
+
+			RandomActivator();
+		}
+	}
+
+	private void RandomActivator()
+	{
+
+	}
 	
 	#endregion
 }
